3ad8426f069f24aeea9953326fe0e0390dc09774
Brennon Junio <juniobrennon@gmail.com>
2023-02-25 14:31:33 -0400

    Simulando erro

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use('/', routes)
 
 app.get('/', (req, res) => {
-  res.send('Olá Mundo 2!!')
+  res.send('Olá Mundo 3!!')
 })
 
 app.listen(port, () => {
--- sum.test.js
+++ sum.test.js
@@ -4,5 +4,5 @@ const sum = require('./sum')
 
 test('Soama de dois valores',()=>{
     const resultado  = sum(2,5)
-    expect(resultado).toBe(7)
+    expect(resultado).toBe(8)
 })
